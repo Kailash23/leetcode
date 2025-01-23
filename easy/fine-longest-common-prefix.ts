eebf1bb59e10ba730101b6a6965833e8494aeaa8
Kailash Uniyal <kailashuniyal04@gmail.com>
2025-01-23 23:27:42 +0530

    Twice the magic that started it all

---
+++ easy/fine-longest-common-prefix.ts
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/longest-common-prefix/
+
+function longestCommonPrefix(strs: string[]): string {
+  let commonPrefix = "";
+  const firstStringIndex = 0;
+  let charIndex = 0;
+
+  for (let i = 0; i < strs[firstStringIndex].length; i++) {
+    const currentChar = strs[firstStringIndex][i];
+
+    for (let strIndex = 0; strIndex < strs.length; strIndex++) {
+      if (strs[strIndex][charIndex] === currentChar) {
+        if (strIndex === strs.length - 1) {
+          commonPrefix += currentChar;
+          charIndex += 1;
+        }
+      } else {
+        return commonPrefix;
+      }
+    }
+  }
+  return commonPrefix;
+}
